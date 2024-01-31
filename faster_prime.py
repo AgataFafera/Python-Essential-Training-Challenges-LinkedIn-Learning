aa4c99a8effba5103fb9751c224b8538d8522132
Agata Fąfera <129389777+AgataFafera@users.noreply.github.com>
2024-01-31 21:16:53 +0100

    Add files via upload

---
+++ faster_prime.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[46]:
+
+
+# Write a function that returns a list of all primes up to a given number.
+# For each number, in order to determine if it is prime, take the following steps:
+# 1. Find the square root of the number
+# 2. Find all the primes up to that square root
+# 3. Test to see if any of those primes are divisors 
+# If a number has no prime divisors, it is prime!
+
+from math import sqrt
+
+def allPrimesUpTo(num):
+    divisors = []
+    
+    if num <= 1: 
+        return False
+    
+    square = int(sqrt(num))
+    for i in range(2,square+1):
+        if num % i == 0:
+                divisors.append(i)
+                
+                for j in divisors:
+                    if num % j == 0:
+                        return False
+                    else:
+                        return True
+            
+
+
+print(allPrimesUpTo(1000))
+print(allPrimesUpTo(2137))
+print(allPrimesUpTo(100))
+print(allPrimesUpTo(13))
+print(allPrimesUpTo(2000))
+
+
+# In[40]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
